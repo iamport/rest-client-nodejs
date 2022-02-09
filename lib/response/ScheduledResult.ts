708cd576301b78695a78e194044d92ae3a44390b
ChulHee Lee (이철희) <leechulhee95@gmail.com>
2022-02-09 11:04:29 +0900

    Schedule 관련 모델과 인터페이스 최신화

--- lib/response/ScheduledResult.ts
+++ lib/response/ScheduledResult.ts
@@ -4,7 +4,10 @@ import { ScheduledStatusEnum } from '../enum';
 class ScheduledResult extends ResponseBase {
   public customer_uid?: string;
   public merchant_uid?: string;
+  public imp_uid?: string;
   public schedule_at?: Date | number;
+  public executed_at?: Date | number;
+  public revoked_at?: Date | number;
   public amount?: number;
   public name?: string;
   public buyer_name?: string;
@@ -14,12 +17,17 @@ class ScheduledResult extends ResponseBase {
   public buyer_postcode?: string;
   public custom_data?: string;
   public schedule_status?: ScheduledStatusEnum;
+  public payment_status?: 'paid' | 'failed' | 'cancelled';
+  public fail_reason?: string;
 
   public setAttributes(response: ScheduledResult): void {
     const {
       customer_uid,
       merchant_uid,
+      imp_uid,
       schedule_at,
+      executed_at,
+      revoked_at,
       amount,
       name,
       buyer_name,
@@ -29,11 +37,16 @@ class ScheduledResult extends ResponseBase {
       buyer_postcode,
       custom_data,
       schedule_status,
+      payment_status,
+      fail_reason,
     } = response;
 
     this.customer_uid = customer_uid;
     this.merchant_uid = merchant_uid;
+    this.imp_uid = imp_uid;
     this.schedule_at = this.timeToDate(schedule_at);
+    this.executed_at = this.timeToDate(executed_at);
+    this.revoked_at = this.timeToDate(revoked_at);
     this.amount = amount;
     this.name = name;
     this.buyer_name = buyer_name;
@@ -43,6 +56,8 @@ class ScheduledResult extends ResponseBase {
     this.buyer_postcode = buyer_postcode;
     this.custom_data = custom_data;
     this.schedule_status = schedule_status;
+    this.payment_status = payment_status;
+    this.fail_reason = fail_reason;
   }
 }
 
